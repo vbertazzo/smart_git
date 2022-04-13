5efa9ca1715cd978aac7d47189f2c21f792471cc
vbertazzo <vbertazzo@gmail.com>
2022-04-13 17:23:20 -0300

    Add repo_detail component

--- lib/smart_git_web/live/page/repo_detail/repo_detail.ex
+++ lib/smart_git_web/live/page/repo_detail/repo_detail.ex
@@ -2,4 +2,18 @@ defmodule SmartGitWeb.Page.RepoDetail do
   @moduledoc false
 
   use SmartGitWeb, :live_component
+
+  def update(assigns, socket) do
+    {:ok, socket |> assign(assigns) |> assign(message: nil)}
+  end
+
+  def handle_event("add-repo", _, socket) do
+    message =
+      case is_nil(socket.assigns.message) do
+        true -> "Repo added!"
+        false -> nil
+      end
+
+    {:noreply, assign(socket, message: message)}
+  end
 end
