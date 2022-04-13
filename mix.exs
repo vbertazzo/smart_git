5090b609d567418fdb9ab5cf8bbfec70f4ad6f34
vbertazzo <vbertazzo@gmail.com>
2022-04-13 18:37:54 -0300

    Add tesla

--- mix.exs
+++ mix.exs
@@ -63,7 +63,9 @@ defmodule SmartGit.MixProject do
       {:excoveralls, "~> 0.10", only: :test},
       {:sobelow, "~> 0.8", only: :dev},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
